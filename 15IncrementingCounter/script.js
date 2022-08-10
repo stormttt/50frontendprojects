dbd3bc5df05eba1353051dea08dd18867df505b7
stromttt <xyxiaoYU1992@gmail.com>
2022-08-10 22:33:37 +0800

    15 finish

---
+++ 15IncrementingCounter/script.js
@@ -0,0 +1,18 @@
+const counters = document.querySelectorAll('.counter');
+counters.forEach( counter =>{
+    counter.innerText = '0'
+    const updateCounter = () =>{
+        const target = Number(counter.getAttribute('data-target'))
+        const c = +counter.innerText;
+
+        const increment = target / 200;
+        if(c < target){
+            counter.innerText = `${Math.ceil(c+increment)}`
+            setTimeout(updateCounter,2)
+        }
+        else{
+            counter.innerText = target
+        }
+    }
+    updateCounter()
+})
